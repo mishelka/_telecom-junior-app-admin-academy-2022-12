f19a8af4a700afa0e96745dfa1dabaa1264d15af
Michaela Bačíková <bacikova.michaela@gmail.com>
2023-01-02 13:56:14 +0100

    constraints

--- prikazy.sql
+++ prikazy.sql
@@ -1,4 +1,15 @@
--- DROP TABLE person;
+DROP TABLE IF EXISTS person;
+
+CREATE TABLE person(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(20) NOT NULL,
+  surname VARCHAR(30) NOT NULL,
+  age INT NOT NULL CHECK(age > 0),
+  birth_date DATE,
+  address VARCHAR(50),
+  identification_number UUID NOT NULL UNIQUE,
+  salary DECIMAL(18,2) DEFAULT 5000.00 CHECK(salary > 0)
+);
 
 /*
 jfkldsa
@@ -17,16 +28,6 @@ jflds
 -- 	PRIMARY KEY(name, surname)
 -- );
 
--- CREATE TABLE person(
---   id INT PRIMARY KEY NOT NULL,
---   name VARCHAR(20) NOT NULL,
---   surname VARCHAR(30) NOT NULL,
---   age INT NOT NULL,
---   birth_date DATE,
---   address VARCHAR(50),
---   salary REAL
--- );
-
 
 -- CREATE TABLE color(
 --     id INT GENERATED ALWAYS AS IDENTITY,
@@ -40,4 +41,4 @@ jflds
 --     primary key(id)
 -- );
 
-drop table if exists color_serial, color;
+-- drop table if exists color_serial, color;
