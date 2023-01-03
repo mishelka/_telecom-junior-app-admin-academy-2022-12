5a8770d813a3db3cc656fab4c1d0f174dd41b33e
Michaela Bačíková <bacikova.michaela@gmail.com>
2023-01-03 09:10:45 +0100

    domaca uloha

--- 02-data-manipulation.sql
+++ 02-data-manipulation.sql
@@ -19,3 +19,35 @@ DELETE FROM category
 WHERE category_id = 19 OR category_id = 20;
 
 SELECT * FROM category;
+
+--domaca uloha
+
+DROP TABLE IF EXISTS links, urls;
+
+CREATE TABLE links(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(20),
+    url VARCHAR(50)
+);
+
+INSERT INTO links (name, url)
+    VALUES
+        ('Facebook', 'www.facebook.com'),
+        ('LinkedIn', 'www.linkedin.com'),
+        ('CDE', 'www.cde.sk');
+
+ALTER TABLE links RENAME TO urls;
+
+UPDATE urls
+SET name = 'Microsoft'
+WHERE id = 2;
+
+--pozor, vymaze vsetko!
+--DELETE FROM urls;
+--TRUNCATE urls;
+
+DELETE FROM urls WHERE id = 1;
+
+SELECT * FROM urls;
+
+
