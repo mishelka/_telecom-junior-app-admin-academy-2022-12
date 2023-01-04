b524fa6da3c8a2c6b9c62172c688c8fae3bcc492
Michaela Bačíková <bacikova.michaela@gmail.com>
2023-01-04 16:12:20 +0100

    agregaty uloha 2

--- 05-agregacne-funkcie.sql
+++ 05-agregacne-funkcie.sql
@@ -25,4 +25,24 @@ join category c on fc.category_id = c.category_id
 group by c.name
 order by average_price desc;
 
+-- 1. Vypiste sumarne, kolko filmov sa nachadza v jednotlivych kategoriach.
+--     Pre overenie vedzte, ze v kategorii Sports sa nachadza 74 filmov a v kategorii Music 51.
+select c.name, count(fc.film_id)
+from category c
+inner join film_category fc on c.category_id = fc.category_id
+inner join film f on fc.film_id = f.film_id
+group by c.category_id, c.name
+order by count(fc.film_id) desc;
 
+--2. Zistite čas v minutach najdlhsieho filmu, najkratsieho filmu a priemerný čas filmov v kategorii Sci-Fi.
+-- Priemerny cas zaokruhlite na 2 desatinne miesta.
+-- Pre kontrolu vedzte, ze max je 51, min 185 a priemerny cas v Sci-Fi je 108,2.
+select c.name, MAX(f.length),
+       MIN(f.length),
+       ROUND(AVG(f.length), 2)
+from film f
+join film_category fc on f.film_id = fc.film_id
+join category c on fc.category_id = c.category_id
+--where c.name LIKE 'Sci-Fi'
+group by c.name
+having AVG(f.length) > 110;
